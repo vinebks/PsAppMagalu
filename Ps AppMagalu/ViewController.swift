658e0f0825e81c0cdea8de9d7145c69d08e01615
Vinicius Santos <60713208+vinebks@users.noreply.github.com>
2021-06-09 19:12:57 -0300

    Add project to git

---
+++ Ps AppMagalu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ps AppMagalu
+//
+//  Created by Usuário Convidado on 09/06/21.
+//  Copyright © 2021 Usuário Convidado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
