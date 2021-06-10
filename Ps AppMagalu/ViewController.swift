28199a9d54a641710ffc097d15089b33b62a7070
Vinicius Santos <60713208+vinebks@users.noreply.github.com>
2021-06-10 01:57:22 -0300

    Implements components

--- Ps AppMagalu/ViewController.swift
+++ Ps AppMagalu/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leading: NSLayoutConstraint!
+    @IBOutlet weak var trailing: NSLayoutConstraint!
+    
+    var menuOut = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   
+    @IBAction func menuTapped(_ sender: Any) {
+        if menuOut == false    {
+            
+            
+            trailing.constant = -200
+            leading.constant = 200
+            menuOut = true
+            
+        }else {
+            leading.constant = 0
+            trailing.constant = 0
+            menuOut = false
+           
+        }
+        
+        UIView.animate(withDuration: 	0.2, delay: 0.0, options: .curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+        }) { (animationComplete) in
+            print("The Animation is Complete")
+        }
+    }
+    
+    
+    
 }
 
